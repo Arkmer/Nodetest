e6a2d10ec35d6ec60695edfb92638d6d345738ab
Ryan Udelhoven <Ryan@Udelhoven.net>
2018-02-09 16:12:48 -0600

    minor line change

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,18 @@
-$.ajax({
-  type: 'GET',
-  url: '/test',
-}).done(function(data){
-    $('h1').text(data);
-  }).fail(function(error){
-    console.log('The "/hello" ajax get request failed with error: ', error);
+console.log('JS')
+$(document).ready(onReady);
+
+function onReady(){
+  $('#bDoIt').on('click', fDoIt);
+}
+
+// This is the function that runs when you press the button! Go to line 14 in server.js
+function fDoIt(){
+  $.ajax({
+    type: 'GET',
+    url: '/test', // This matches the other url in the server. This is what links them!
+  }).done(function(data){
+      $('h1').text(data);
+    }).fail(function(error){
+      console.log('The "/hello" ajax get request failed with error: ', error);
   });
+} // end fDoIt
--- server/server.js
+++ server/server.js
@@ -4,17 +4,18 @@
 // Express code.
 let express = require('express');
 let app = express();
-const test1 = require('./modules/test');
+const test1 = require('./modules/test'); // This comes from our module and brings the whole function!
 // Tells express where our static content is located.
 app.use(express.static('server/public'));
 
 // Can use any number from 1000 - 99999
 // but don't choose 5432 which is the default for the database.
 const port = 5000;
-app.get('/test', function(request, response){
-  response.send(test1)
+app.get('/test', // This matches the other url in the client. This is what links them!
+    function(request, response){
+  response.send(test1) // This test matches the one in line 7!
+}); // This is where ajax takes me
 
-});
 // Start our server.
 app.listen(port, function() {
     console.log(`Server listening on port ${port}`);
